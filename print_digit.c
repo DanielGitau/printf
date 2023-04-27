1c12357913a0ec0700f8984851d02564d2162550
SARK <kosinick01@gmail.com>
2023-04-27 05:37:33 -0700

    updated print_digit file

--- print_digit.c
+++ print_digit.c
@@ -3,45 +3,25 @@
  * print_digit - handles the int format specifier
  * @num: the int to be formatted
  * Return: the num of int outputted
-g **/
+ **/
 int print_digit(long int num)
 {
-	int digit, temp, count, end, expo;
+	int n = 0;
+	int num_digit = 0;
 
-	expo = 1;
-	count = 0;
-	
 	if (num < 0)
 	{
 		_putchar('-');
-		count++;
-		num = -num;
+		num = -(num);
+		num_digit++;
 	}
 
-	end = num % 10;
-	num = num / 10;
-	temp = num;
-	
-	if (temp > 0)
-	{
-		while (temp / 10 != 0)
-		{
-			expo = expo * 10;
-			temp = temp / 10;
-		}
-		temp = num;
+	if (num >= 10)
+		num_digit += print_digit(num / 10);
 
-		while (expo > 0)
-		{
-			digit = temp / expo;
-			_putchar(digit + '0');
-			temp = temp - (digit * expo);
-			expo = expo / 10;
-			count++;
-		}
-	}
-	_putchar(end + '0');
-	count++;
+	n = num % 10;
+	_putchar(n + '0');
+	num_digit++;
 
-	return(count);
+	return (num_digit);
 }
